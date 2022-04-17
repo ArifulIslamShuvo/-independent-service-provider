0f8c7bdc40bf0f3410069a2709094f49541feefe
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-17 22:41:49 +0600

    logIn is working

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,14 +2,39 @@ import React from "react";
 import "./AuthForm.css";
 import GoogleLogo from "../../../imagea/google.svg"
 import { useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if (user) {
+    navigate('/');
+    console.log(user);
+  }
+ 
+
+  const handleLogout = event => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    signInWithEmailAndPassword(email, password)
+  }
+
+
   return (
     <div className='container auth-form-container'>
       <div className='auth-form'>
         <h1 className="text-info m-4">Login</h1>
-        <form >
+        <form onSubmit={handleLogout}>
           <div className='input-field'>
             <label htmlFor='email'>Email</label>
             <div className='input-wrapper'>
